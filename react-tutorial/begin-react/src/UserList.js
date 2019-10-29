5d0efbfd77b8d5f1f4e94195ff82b5583b033f77
DevRappers <devrappers@gmail.com>
2019-10-29 16:08:45 +0900

    useEffect를 사용하여 마운트/언마운트/업데이트 시 할 작업 설정하기

--- react-tutorial/begin-react/src/UserList.js
+++ react-tutorial/begin-react/src/UserList.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+/*
+useEffect Hook
+우리가 만든 리액트컴포넌트가 처음 화면에 나타나게 될때 화면에서 사라지게 될때 작업을 할 수 있고 업데이트 될때에 작업을 할 
+수 있게 해주는 함수 
+
+컴포넌트가 마운트될때 언마운트될때 사용할 수 있음. 
+마운트될때 그냥 함수 / 삭제될때 return 
+*/
 
 function User({ user, onRemove, onToggle}){
     const {username, email, id, active} = user;
+    // useEffect(() =>{
+    //     console.log('컴포넌트가 화면에 나타남');
+    //     // props -> state 
+    //     // REST API : 외부 API요청
+    //     // D3 Video.js : 라이브러리 사용
+    //     // setInterval, setTimeout
+    //     return () => {
+    //         // clearInterval, clearTimeout
+    //         // 라이브러리 인스턴스 제거 
+    //         console.log('컴포넌트가 화면에서 사라짐');
+    //     };
+    // }, []);
+    // 업데이트 될때 사용됨 [user]로 주면 / deps배열에 어떤값을 넣게 되면 해당값이 바뀔때마다 함수가 호출됨. 
+    // deps배열을 생략하면 리랜더링되고 호출됨. 모든 컴포넌트가 출력됨 해당컴포넌트가 출력되는것이 아님.
+    useEffect(() => {
+        console.log('user값이 설정됨 ')
+        console.log(user);
+        return () => {
+            console.log('user 값이 바뀌기 전')
+            console.log(user);
+        }
+    },[user]);
     return(
         <div>
             <b style={{
