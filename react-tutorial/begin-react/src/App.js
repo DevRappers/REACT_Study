2157a49bd929934a4e6a9ae2f96b76dbe180148a
DevRappers <devrappers@gmail.com>
2019-10-30 10:44:26 +0900

    커스텀 Hook 만들어서 사용하기

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,6 +1,7 @@
 import React, { useRef, useReducer, useMemo, useCallback} from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import useInputs from './useInputs';
 
 function countActiveUsers(users){
   console.log('활성 사용자 수를 세는중...');
@@ -9,10 +10,6 @@ function countActiveUsers(users){
 
 // 초기값 설정
 const initialState = {
-  inputs:{
-    username: '',
-    email: ''
-  },
   users:[
     {
       id: 1,
@@ -38,14 +35,6 @@ const initialState = {
 function reducer(state, action)
 {
   switch(action.type){
-    case 'CHANGE_INPUT':
-      return{
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
     case 'CREATE_USER':
       return{
         inputs: initialState.inputs,
@@ -73,18 +62,15 @@ function reducer(state, action)
 function App() {
   // initalState가 기본값이기때문에 state에서 불러올 수 있음 값의 변경은 reducer
   const [state,dispatch] = useReducer(reducer, initialState);
+  const [form, onChange, reset] = useInputs({
+    username: '',
+    email: ''
+  });
+  
+  const {username, email} = form;
+
   const nextId = useRef(4);
   const {users} = state;
-  const {username, email} = state.inputs;
-
-  const onChange = useCallback(e => {
-    const {name, value} = e.target;
-    dispatch({
-      type: 'CHANGE_INPUT',
-      name,
-      value
-    })
-  }, []);
 
   const onCreate = useCallback(() => {
     dispatch({
@@ -96,7 +82,8 @@ function App() {
       }
     })
     nextId.current += 1;
-  }, [username, email]);
+    reset();
+  }, [username, email,reset]);
 
   const onToggle = useCallback(id => {
     dispatch({
