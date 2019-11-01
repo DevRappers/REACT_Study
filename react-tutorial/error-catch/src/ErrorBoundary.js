c00e1d6e8dc146816230ac75f27f2181aee75f0d
DevRappers <devrappers@gmail.com>
2019-11-01 09:38:35 +0900

    Prettier 사용

--- react-tutorial/error-catch/src/ErrorBoundary.js
+++ react-tutorial/error-catch/src/ErrorBoundary.js
@@ -1,32 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import * as Sentry from '@sentry/browser';
 
-class ErrorBoundary extends Component{
-    state = {
-        error: false
-    };
+class ErrorBoundary extends Component {
+	state = {
+		error: false
+	};
 
-    // error : 어떤에러, info : 어디서 에러가 발생했는지
-    componentDidCatch(error, info){
-        console.log('에러가 발생했습니다.');
-        console.log({
-            error,
-            info
-        });
-        this.setState({
-            error: true,
-        });
+	// error : 어떤에러, info : 어디서 에러가 발생했는지
+	componentDidCatch(error, info) {
+		console.log('에러가 발생했습니다.');
+		console.log({
+			error,
+			info
+		});
+		this.setState({
+			error: true
+		});
 
-        if(process.env.NODE_ENV === 'production'){
-            Sentry.captureException(error, {extra:info});
-        }
-    }
-    render() {
-        if(this.state.error){
-            return <h1>에러 발생!</h1>
-        }
-        return this.props.children;
-    }
+		if (process.env.NODE_ENV === 'production') {
+			Sentry.captureException(error, { extra: info });
+		}
+	}
+	render() {
+		if (this.state.error) {
+			return <h1>에러 발생!</h1>;
+		}
+		return this.props.children;
+	}
 }
 
 export default ErrorBoundary;
