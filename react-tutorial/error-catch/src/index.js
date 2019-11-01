263495b9b5fc6d589dab1809026c13cc86d15b03
DevRappers <devrappers@gmail.com>
2019-11-01 09:25:13 +0900

    Sentry연동

--- react-tutorial/error-catch/src/index.js
+++ react-tutorial/error-catch/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({dsn: "https://590c178f63104b13abe3dc8281b8b4a4@sentry.io/1804384"});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
